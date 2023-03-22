9f68747cf3532dd0152e0f6c0de4d357e92dbf16
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-03-22 16:01:27 +0300

    Python file

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+from compress import compress_image
 
 app = Flask(__name__)
+
+@app.route('/compress', methods=['POST'])
+def compress():
+    file = request.files['image']
+    compressed_file = compress_image(file)
+    return send_file(compressed_file, attachment_filename='compressed.jpg', as_attachment=True)
+
+@app.route('/example')
+def example():
+    return send_file('example.jpg', attachment_filename='example.jpg', as_attachment=True)
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template(index.html)
 
 if __name__ == '__main__':
     app.run(debug=True)
