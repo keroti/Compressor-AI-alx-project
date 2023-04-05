e29be7f815d332690bd67544f5556fc3cb6f4202
keroti <peterkeroti55@gmail.com>
2023-04-05 09:05:42 +0300

    Project update

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
+from forms import RegistrationForm, LoginForm
+import requests
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['MEGAOPTIM_API_KEY'] = os.environ.get('LxT2Vq6WFS7qmX7aPG8ok1FYjeraiQjj')
+app.config['MEGAOPTIM_API_HOST'] = 'megaoptim-image-compression.p.rapidapi.com'
 
 @app.route("/")
-
+@app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route('/compress-image', methods=['POST'])
+def compress_image():
+    api_key = app.config['MEGAOPTIM_API_KEY']
+    api_host = app.config['MEGAOPTIM_API_HOST']
+    api_url = 'https://megaoptim-image-compression.p.rapidapi.com/optimize'
+
+    # Get the uploaded image file
+    image_file = request.files.get('image')
+
+    # Compress the image using the MegaOptim API
+    headers = {
+        'X-RapidAPI-Host': api_host,
+        'X-RapidAPI-Key': api_key,
+        'Content-Type': 'image/jpeg'  # Change this if your images are in a different format
+    }
+    response = requests.post(api_url, headers=headers, files={'file': image_file})
+
+    # Return the compressed image data as a JSON response
+    if response.status_code == 200:
+        response_data = response.json()
+        return jsonify({'status': 'success', 'data': response_data['data'], 'format': response_data['format']})
+    else:
+        return jsonify({'status': 'error', 'message': 'Failed to compress image'})
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'peterkeroti55@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/preview.js
+++ static/preview.js
@@ -1,23 +1,39 @@
-const imageInput = document.getElementById('image-input');
-const imagePreview = document.getElementById('image-preview');
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('upload-form');
+  const imagePreview = document.getElementById('image-preview');
 
-imageInput.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-  const url = URL.createObjectURL(file);
-  const img = new Image();
-
-  img.onload = function() {
-    const imgWidth = img.width;
-    const imgHeight = img.height;
-    const imgSize = (file.size / 1024).toFixed(2);
-    const imgURL = url;
-
-    imagePreview.innerHTML = `
-      <img src="${imgURL}" alt="Selected Image" width="${imgWidth}" height="${imgHeight}">
-      <p>Image Size: ${imgSize} KB</p>
-      <p>Image URL: ${imgURL}</p>
-    `;
+  const app = {
+    config: {
+      RAPIDAPI_API_KEY: 'your_api_key_here'
+    }
   };
 
-  img.src = url;
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const image = event.target.image.files[0];
+    const formData = new FormData();
+    formData.append('image', image);
+
+    fetch('/compress-image', {
+      method: 'POST',
+      headers: {
+        'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com',
+        'X-RapidAPI-Key': app.config['RAPIDAPI_API_KEY'],
+      },
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'success') {
+        const compressedImage = new Image();
+        compressedImage.src = `data:image/${data.format};base64,${data.data}`;
+        imagePreview.appendChild(compressedImage);
+      } else {
+        console.error('Error: ' + data.message);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  });
 });
