5b91e01b63dd1c1d697d126da21f2b6f5918f3b0
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-04-11 09:15:10 +0300

    Javascript to preview images

--- static/preview.js
+++ static/preview.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  const imageInput = $('#image-input');
+  const imagePreview = $('#image-preview');
+
   // When an image is selected
-  $('#image-input').on('change', function () {
-      var files = $(this).prop('files');
-      $('#image-preview').html('');
-      $.each(files, function (index, file) {
-          var reader = new FileReader();
-          reader.onload = function (event) {
-              var img = $('<img>', {
-                  class: 'image-previews',
-                  src: event.target.result
-              });
-              $('#image-preview').append(img);
-          };
-          reader.readAsDataURL(file);
-      });
+  imageInput.on('change', function() {
+    const files = $(this).prop('files');
+    imagePreview.html('');
+
+    $.each(files, function(index, file) {
+      const reader = new FileReader();
+
+      reader.onload = function(event) {
+        const img = $('<img>', {
+          class: 'image-previews',
+          src: event.target.result
+        });
+
+        imagePreview.append(img);
+      };
+
+      reader.readAsDataURL(file);
+    });
   });
 });
