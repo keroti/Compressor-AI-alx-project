a87f9ed41923c45cab47d4d8a466c59658f78c5d
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-04-11 09:40:37 +0300

    Javascript to compress image

--- static/main.js
+++ static/main.js
@@ -46,24 +46,34 @@ $(document).ready(function () {
   
   function pollForResult(processId) {
     fetch(`https://megaoptim-image-compression.p.rapidapi.com/optimize/${processId}/result?timeout=120`, {
-      method: 'POST', // Change the method to POST
+      method: 'GET',
       headers: {
         'X-API-KEY': 'LxT2Vq6WFS7qmX7aPG8ok1FYjeraiQjj',
-        'X-RapidAPI-Key': 'a550036d08msh0d1430c3f64599cp15b8aajsn46bd9d79d7c7',
+        'X-RapidAPI-Key': app.config['RAPIDAPI_API_KEY'],
         'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com'
       }
     })
-    .then(response => response.text()) // Get the response as text instead of JSON
+    .then(response => response.json())
     .then(data => {
-      if (data.includes('<')) {
-        // If the response contains '<', it is not valid JSON
-        console.error('Error: invalid response');
-      } else {
-        // Otherwise, parse the JSON and display the compressed image
-        const compressedData = JSON.parse(data);
+      if (data.status === 'success') {
         const compressedImage = new Image();
-        compressedImage.src = `data:image/${compressedData.format};base64,${compressedData.data}`;
-        $('#image-preview').html(compressedImage);
+        compressedImage.src = `data:image/${data.format};base64,${data.data}`;
+        imagePreview.appendChild(compressedImage);
+
+        // Add download button
+        const downloadButton = document.createElement('a');
+        downloadButton.textContent = 'Download Image';
+        downloadButton.download = `compressed.${data.format}`;
+        downloadButton.href = compressedImage.src;
+        imagePreview.appendChild(downloadButton);
+      } else if (data.status === 'processing') {
+        // The compressed image data is still processing, so wait a bit and try again
+        setTimeout(() => {
+          pollForResult(processId);
+        }, 1000);
+      } else {
+        // Something went wrong, so display an error message
+        console.error('Error: ' + data.error);
       }
     })
     .catch(error => {
