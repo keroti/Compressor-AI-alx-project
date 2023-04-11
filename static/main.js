a92fa7f729390a64018516e52e9c6fa084a46e57
Peter Keroti <59761453+keroti@users.noreply.github.com>
2023-04-11 09:35:20 +0300

    Javascript to compress image

--- static/main.js
+++ static/main.js
@@ -1,65 +1,73 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('upload-form');
-  const imagePreview = document.getElementById('image-preview');
-
+$(document).ready(function () {
   const app = {
     config: {
-      RAPIDAPI_API_KEY: 'LxT2Vq6WFS7qmX7aPG8ok1FYjeraiQjj'
+      RAPIDAPI_API_KEY: 'a550036d08msh0d1430c3f64599cp15b8aajsn46bd9d79d7c7'
     }
   };
-
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    const image = event.target.image.files[0];
-    const formData = new FormData();
-    formData.append('image', image);
-
-    fetch('https://megaoptim-image-compression.p.rapidapi.com/optimize', {
-      method: 'POST',
+  
+  // When an image is selected
+  $('#image-input').on('change', function () {
+    const files = $(this).prop('files');
+    $('#image-preview').html('');
+    $.each(files, function (index, file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const img = $('<img>', {
+          class: 'image-previews',
+          src: event.target.result
+        });
+        $('#image-preview').append(img);
+        
+        // Compress the image and display the result
+        const formData = new FormData();
+        formData.append('image', file);
+        
+        fetch('https://megaoptim-image-compression.p.rapidapi.com/optimize', {
+          method: 'POST',
+          headers: {
+            'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com',
+            'X-RapidAPI-Key': app.config['RAPIDAPI_API_KEY'],
+            'Content-Type': 'image/jpeg'
+          },
+          body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+          // Poll for the result of the compression
+          pollForResult(data.process_id);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      };
+      reader.readAsDataURL(file);
+    });
+  });
+  
+  function pollForResult(processId) {
+    fetch(`https://megaoptim-image-compression.p.rapidapi.com/optimize/${processId}/result?timeout=120`, {
+      method: 'POST', // Change the method to POST
       headers: {
-        'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com',
-        'X-RapidAPI-Key': app.config['RAPIDAPI_API_KEY'],
-        'Content-Type': 'image/jpeg'
-      },
-      body: formData
+        'X-API-KEY': 'LxT2Vq6WFS7qmX7aPG8ok1FYjeraiQjj',
+        'X-RapidAPI-Key': 'a550036d08msh0d1430c3f64599cp15b8aajsn46bd9d79d7c7',
+        'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com'
+      }
     })
-    .then(response => response.json())
+    .then(response => response.text()) // Get the response as text instead of JSON
     .then(data => {
-      pollForResult(data.process_id);
+      if (data.includes('<')) {
+        // If the response contains '<', it is not valid JSON
+        console.error('Error: invalid response');
+      } else {
+        // Otherwise, parse the JSON and display the compressed image
+        const compressedData = JSON.parse(data);
+        const compressedImage = new Image();
+        compressedImage.src = `data:image/${compressedData.format};base64,${compressedData.data}`;
+        $('#image-preview').html(compressedImage);
+      }
     })
     .catch(error => {
       console.error(error);
     });
-
-    function pollForResult(processId) {
-      fetch(`https://megaoptim-image-compression.p.rapidapi.com/optimize/${processId}/result?timeout=120`, {
-        method: 'GET',
-        headers: {
-          'X-API-KEY': 'LxT2Vq6WFS7qmX7aPG8ok1FYjeraiQjj',
-          'X-RapidAPI-Key': 'a550036d08msh0d1430c3f64599cp15b8aajsn46bd9d79d7c7',
-          'X-RapidAPI-Host': 'megaoptim-image-compression.p.rapidapi.com'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.status === 'success') {
-          const compressedImage = new Image();
-          compressedImage.src = `data:image/${data.format};base64,${data.data}`;
-          imagePreview.appendChild(compressedImage);
-        } else if (data.status === 'processing') {
-          // The compressed image data is still processing, so wait a bit and try again
-          setTimeout(() => {
-            pollForResult(processId);
-          }, 1000);
-        } else {
-          // Something went wrong, so display an error message
-          console.error('Error: ' + data.error);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }
-  });
+  }
 });
